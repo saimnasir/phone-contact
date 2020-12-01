d8b16e89ec92762538c6bb575c7c82f1eac054af
saimnasir <saim.nasir.65@gmail.com>
2020-12-01 22:27:28 +0300

    reporting mapper addes

---
+++ Reporting/Map/AutoMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace PhoneContact.Map
+{
+    public class AutoMappingProfile : Profile
+    {
+        public AutoMappingProfile()
+        {
+            ViceVersa<DataModels.Report, ViewModels.Report>();
+            ViceVersa<DataModels.ReportDetail, ViewModels.ReportDetail>();
+        }
+        protected virtual void ViceVersa<T1, T2>()
+        {
+            CreateMap<T1, T2>();
+            CreateMap<T2, T1>();
+        }
+    }
+}
