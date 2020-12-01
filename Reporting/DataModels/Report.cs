845537c58b3d1b85ccdc5780887a363eadf224e8
saimnasir <saim.nasir.65@gmail.com>
2020-12-01 22:23:38 +0300

    Reporting Datamodels added

---
+++ Reporting/DataModels/Report.cs
@@ -0,0 +1,9 @@
+﻿using Core.Enums;
+
+namespace DataModels
+{
+    public class Report : DataModel
+    {
+        public ReportStatuses Status { get; set; }
+    }
+}
---
+++ Reporting/DataModels/ReportDetail.cs
@@ -0,0 +1,19 @@
+﻿using DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataModels
+{
+    public class ReportDetail: DataModel
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string CompanyName { get; set; }
+        public string Lattitude { get; set; }
+        public string Longitude { get; set; }
+
+    }
+}
